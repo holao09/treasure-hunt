e03dad7492a3f90480e39341a534c0bc64800c76
Việt Nguyễn <mrholao09@gmail.com>
2021-10-17 17:18:51 +0700

    Update script.ts

--- script.ts
+++ script.ts
@@ -42,7 +42,9 @@ const run = async () => {
 
     console.log(`[${i}] Email:`, username + '@emaildrop.io');
 
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    });
     const page = await browser.newPage();
     const screenshot = makeScreenshot(page, username);
 
